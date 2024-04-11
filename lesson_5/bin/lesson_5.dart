09ad04121b34e8aa5cda1d64007f5e4732b15452
Filipe Plucenio <plucenio@gmail.com>
2024-04-11 17:29:38 -0300

    improvements on lesson 5.

--- lesson_5/bin/lesson_5.dart
+++ lesson_5/bin/lesson_5.dart
@@ -1,12 +1,18 @@
 void main(List<String> arguments) {
   final datetime = DateTime.now();
-  print(solution(1, 10, 2));
+  print(solution(1, 100000000, 2));
   print(DateTime.now().difference(datetime));
 }
 
 int solution(int A, int B, int K) {
+  int sum = 1;
   if (A > B || K == 0) return 0;
-  final list = List.generate((B - A) + 1, (index) => index + A)
-    ..removeWhere((element) => element % K != 0);
-  return list.length;
+  int count = 0;
+  for (int i = A; i <= B; i = i + sum) {
+    if (i % K == 0) {
+      count++;
+      sum = K;
+    }
+  }
+  return count;
 }
