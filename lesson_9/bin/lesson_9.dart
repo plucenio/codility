4938fb4e75021e6a6d6026941504ff16703f889f
Filipe Plucenio <plucenio@gmail.com>
2024-04-12 19:16:39 -0300

    added lesson 9 improvements.

--- lesson_9/bin/lesson_9.dart
+++ lesson_9/bin/lesson_9.dart
@@ -8,17 +8,20 @@ void main(List<String> arguments) {
 
 int solution(List<int> A) {
   int greaterSum = 0;
+  int greaterZ = 0;
   for (int x = 0; x <= A.length; x++) {
     for (int y = 1; y <= A.length - 3; y++) {
       for (int z = 2; z <= A.length - 2; z++) {
         if (z > y && y > x && x >= 0) {
+          if (greaterZ > A[z - 1]) continue;
+          greaterZ = z - 1;
           HashSet<int> set = HashSet<int>.from([
             A[x + 1],
             A[x + 2],
             A[y - 1],
             A[y + 1],
             A[y + 2],
-            A[z - 1],
+            A[greaterZ],
           ]);
           int number = 0;
           for (var element in set) {
