a941b451509baf63a2970126a754403b4aa96d72
Filipe Plucenio <plucenio@gmail.com>
2024-04-12 17:28:40 -0300

    added lesson 7.

---
+++ lesson_7/bin/lesson_7.dart
@@ -0,0 +1,43 @@
+void main(List<String> arguments) {
+  var dateTime = DateTime.now();
+  //2
+  print(solution(
+    [7, 20, 2, 5, 1, 3, 8, 9],
+    [0, 1, 0, 1, 0, 0, 0, 0],
+  ));
+
+  //print(solution([7, 3], [1, 0]));
+
+  //print(
+  //  solution(
+  //    List.generate(60000, (index) => index)..shuffle(),
+  //    List.generate(60000, (index) => index % 2 == 0 ? 0 : 1),
+  //  ),
+  //);
+  print(DateTime.now().difference(dateTime));
+}
+
+int solution(List<int> A, List<int> B) {
+  if (A.isEmpty) {
+    return 0;
+  }
+  if (B.toSet().length == 1) {
+    return A.length;
+  }
+  for (int i = 0; i < A.length - 1; i++) {
+    if (B[i] == 1) {
+      if (B[i] != B[i + 1]) {
+        if (A[i] > A[i + 1]) {
+          A.removeAt(i + 1);
+          B.removeAt(i + 1);
+          i = i > 0 ? i - 2 : -1;
+          continue;
+        }
+        A.removeAt(i);
+        B.removeAt(i);
+        i = i > 0 ? i - 2 : -1;
+      }
+    }
+  }
+  return A.length;
+}
