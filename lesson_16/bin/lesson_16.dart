ab675da6bebcf3f54b2130dc74608b8b15dd8624
Filipe Plucenio <plucenio@gmail.com>
2024-04-13 00:15:13 -0300

    added lesson 16 improvements.

--- lesson_16/bin/lesson_16.dart
+++ lesson_16/bin/lesson_16.dart
@@ -9,25 +9,13 @@ int solution(List<int> A, List<int> B) {
   if (A.length <= 1) {
     return A.length;
   }
-  int count = 0;
-  final N = A.length;
-  for (var i = 0; i < N; i++) {
-    if (i == N - 1) {
-      if (B[i - 1] < A[i]) {
-        count++;
-      }
-      break;
+  int lastB = -1;
+  int result = 0;
+  for (int x = 0; x < A.length; x++) {
+    if (A[x] > lastB) {
+      result += 1;
+      lastB = B[x];
     }
-    //Tem interseccão?
-    if (A[i + 1] <= B[i]) {
-      //A linha seguinte é menor que a atual?
-      if (B[i + 1] < B[i]) {
-        count++;
-        continue;
-      }
-      i++;
-    }
-    count++;
   }
-  return count;
+  return result;
 }
