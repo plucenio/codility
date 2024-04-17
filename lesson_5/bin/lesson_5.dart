5cf9bf902a405db0d3cda3a5b526c341520ffb1f
Filipe Plucenio <plucenio@gmail.com>
2024-04-17 19:34:13 -0300

    added lesson 5 improvements.

--- lesson_5/bin/lesson_5.dart
+++ lesson_5/bin/lesson_5.dart
@@ -1,12 +1,14 @@
 void main(List<String> arguments) {
   final datetime = DateTime.now();
-  print(solution(0, 999999999999999999, 2));
+  print(solution(0, 999999999999999999, 56));
   print(DateTime.now().difference(datetime));
 }
 
 int solution(int A, int B, int K) {
   int sum = 1;
+  if (A == 0 && B == 0) return 0;
   if (A > B || K == 0) return 0;
+  if (K > A && K > B) return 0;
   int count = 0;
   for (int i = A; i <= B; i = i + sum) {
     if (sum != 1) {
--- lesson_5/test/lesson_5_test.dart
+++ lesson_5/test/lesson_5_test.dart
@@ -3,19 +3,59 @@ import 'package:test/test.dart';
 import '../bin/lesson_5.dart';
 
 void main() {
-  test('A greater than B should return 0', () {
+  test('Test 1', () {
     expect(solution(5, 3, 2), equals(0));
   });
 
-  test('K is 0 should return 0', () {
+  test('Test 2', () {
     expect(solution(1, 10, 0), equals(0));
   });
 
-  test('All elements divisible by K should return correct length', () {
+  test('Test 3', () {
     expect(solution(3, 10, 3), equals(3));
   });
 
-  test('No elements divisible by K should return 0', () {
+  test('Test 4', () {
     expect(solution(1, 10, 2), equals(5));
+  });
+
+  test('Test 5', () {
+    expect(solution(0, 0, 2), equals(0));
+  });
+
+  test('Test 6', () {
+    expect(solution(0, 0, 0), equals(0));
+  });
+
+  test('Test 7', () {
+    expect(solution(-1, 0, 0), equals(0));
+  });
+
+  test('Test 8', () {
+    expect(solution(-1, -2, 0), equals(0));
+  });
+
+  test('Test 9', () {
+    expect(solution(-1, -2, -3), equals(0));
+  });
+
+  test('Test 10', () {
+    expect(solution(1000, -2, -3), equals(0));
+  });
+
+  test('Test 11', () {
+    expect(solution(10000, 20000, 3), equals(3333));
+  });
+
+  test('Test 12', () {
+    expect(solution(9, 19, 3), equals(4));
+  });
+
+  test('Test 13', () {
+    expect(solution(9, 19, 20), equals(0));
+  });
+
+  test('Test 14', () {
+    expect(solution(0, 1, 11), equals(0));
   });
 }
